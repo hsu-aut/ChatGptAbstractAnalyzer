fc49b033510b9a1c4c394013341bc7e1dcd1e979
mxweigand <mxweigand@gmail.com>
2024-12-16 15:07:47 +0100

    added comments

--- evaluationMethodAnalysis.py
+++ evaluationMethodAnalysis.py
@@ -5,18 +5,18 @@ from pydantic import BaseModel
 from utils import read_bibtex, bibtex_to_dataframe
 import instructor
 
-# Load the API key from the config file
+# load the api key from the config file
 config = configparser.ConfigParser()
 config.read('config.ini')
 api_key = config['API']['api_key']
 
-# Path to the BibTeX file
+# path to the bibtex file
 bibtech_file_path = 'data/data.bib'
 
-# Number of papers to analyze from all the papers
+# number of papers to analyze from all the papers
 num_papers = 20
 
-# Define the data models for structured output
+# define the data models for structured output
 class EvaluationType(BaseModel):
     name: str
     used_in_paper_id: list[int]
@@ -24,12 +24,15 @@ class EvaluationTypeAnalysis(BaseModel):
     evaluation_types: list[EvaluationType]
 
 if __name__ == "__main__":
+
     # read the data
     data = read_bibtex(bibtech_file_path)
     bibtex_data = bibtex_to_dataframe(data)
+
     # select 20 random papers 
     sample = bibtex_data.sample(n=num_papers, random_state=1)
     # print(sample.head(10))
+
     # generate prompts for the selected papers
     paper_list = ""
     for i, row in enumerate(sample.itertuples(), start=1):
@@ -39,7 +42,8 @@ if __name__ == "__main__":
     # remove the extra newlines at the end
     paper_list = paper_list.strip()
     # print(paper_list)
-    # analyze the abstracts
+
+    # create base prompt and combine with the paper list
     base_prompt = """
 Read the following list of papers, containing the paper's title and abstract. 
 Try to analyze how the papers evaluate their work. 
@@ -48,10 +52,14 @@ As a response, provide a list of the used types evalaution, each with the IDs of
     """
     prompt = f"{base_prompt}\n{paper_list}"
     # print(prompt)
+
+    # create the client
     client = instructor.from_openai(
         openai.OpenAI(api_key=api_key),
         mode=instructor.Mode.JSON,
     )
+
+    # execute the prompt
     evaluation_types = client.chat.completions.create(
         model="gpt-4o-mini",
         response_model=EvaluationTypeAnalysis,
